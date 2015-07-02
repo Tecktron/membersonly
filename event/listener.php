61790fa9218d75b8ac2d4b1f889dab6be1472ecb
Tecktron <git@tecktron.net>
2015-07-02 16:09:25 -0400

    Updated braces to coding standard

--- event/listener.php
+++ event/listener.php
@@ -50,13 +50,15 @@ class listener implements EventSubscriberInterface
 	public function redirect_if_not_logged_in($event)
 	{
 		// Check to see if we are already logged in, if we are, bypass all this.
-		if ($this->user->data['user_id'] == ANONYMOUS) {
+		if ($this->user->data['user_id'] == ANONYMOUS)
+		{
 			//The base filename for the login page.
 			$base = "ucp.$this->phpEx";
 			//We use php self server variable since it's the most reliable way
 			$current = $this->request->server('PHP_SELF');
 			//if we are on the page, then we can just return.
-			if (strpos($current, $base) !== false) {
+			if (strpos($current, $base) !== false)
+			{
 				return;
 			}
 			//generate the login url
